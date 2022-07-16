8863b5df0a848c13aed6a9042629091c0c832604
linglingliuTO <linglingliu.to@gmail.com>
2022-07-16 15:29:37 +0000

    add function on assertArraysEqual

---
+++ lotide/assertArraysEqual.js
@@ -0,0 +1,19 @@
+
+
+
+function assertArraysEqual(actual, expected) {
+  var output = true   
+  for (var i = 0; i < actual.length; i++) {
+      if ( actual[i]!== expected[i]) {
+        output = false 
+      } 
+}
+if (output=== true ) {
+  console.log( "Assertion Passed: "+actual+" === "+ expected)
+} else {
+  console.log( `Assertion Failed: ${actual} !== ${expected} `)
+}
+}
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3])
