3c11eeca4c17468b1d75486d82168dcd76ea6b64
linglingliuTO <linglingliu.to@gmail.com>
2022-07-16 15:50:51 +0000

    add flatten function

---
+++ lotide/flatten.js
@@ -0,0 +1,29 @@
+function flatten (array) {
+result = [] 
+  for (var i = 0; i <array.length; i ++) {
+    if (Array.isArray(array[i]) === true)  {
+      for ( var j = 0; j < array[i].length; j++)
+      result.push (array[i][j])
+    } else {
+      result.push (array[i])
+    }
+  }
+  return result 
+}
+
+function assertArraysEqual(actual, expected) {
+  var output = true   
+  for (var i = 0; i < actual.length; i++) {
+      if ( actual[i]!== expected[i]) {
+        output = false 
+      } 
+}
+if (output=== true ) {
+  console.log( "Assertion Passed: "+actual+" === "+ expected)
+} else {
+  console.log( `Assertion Failed: ${actual} !== ${expected} `)
+}
+}
+
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6])
