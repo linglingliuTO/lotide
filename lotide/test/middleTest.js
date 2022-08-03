fc3dd5cd0514d61cb470f135a67e1aac5b3cb91f
linglingliuTO <linglingliu.to@gmail.com>
2022-08-03 23:54:04 +0000

    update lotide

--- lotide/test/middleTest.js
+++ lotide/test/middleTest.js
@@ -1,4 +1,20 @@
 const middle = require('../middle')
-const assertArraysEqual=  require('../assertArraysEqual')
-assertArraysEqual(middle([1, 2, 3]) , [2])
-assertArraysEqual(middle([1, 2, 3, 4]) ,  [2, 3])
+const expect = require('chai').expect;
+
+
+describe("#middle", ()=> {
+
+
+  it("returns [2] for [1,2,3]", () => {
+   expect(middle([1, 2, 3])).to.eql([2]);
+  })
+
+  it("returns  [2,3] for [1,2,3,4]", () => {
+    expect(middle([1, 2, 3,4])).to.eql([2,3]);
+
+  })
+
+
+
+})
+
